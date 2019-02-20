e392814a59e0c3a23956f519ffcb5e7b676f13ea
Slaazy <44474175+Slaazy@users.noreply.github.com>
2019-02-20 20:35:44 -0300

    Add files via upload

---
+++ Seleção de nomes.java
@@ -0,0 +1,37 @@
+
+
+
+import java.util.Scanner;
+
+
+public class Milagre {
+	
+	public static void main(String[] args) {
+		
+		Scanner x = new Scanner(System.in);
+		
+		
+		//Variaveis
+		String nome;
+                
+		
+		System.out.println("Digite alguma coisa");
+		nome = x.nextLine(); 
+		
+		
+		if ("Icaro".equals(nome)) {
+                    System.out.println("Certo");
+                }
+                else
+                    if(!"icaro".equals(nome)){
+                            
+                    System.out.println("Errado");
+                }
+                
+        }
+}
+                
+        
+                      
+        
+
