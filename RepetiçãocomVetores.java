fbc25ee3192c0b3a055629fe2b87d820fe769c07
Slaazy <44474175+Slaazy@users.noreply.github.com>
2019-02-27 13:10:25 -0300

    Add files via upload

---
+++ RepetiçãocomVetores.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Repetição {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner x = new Scanner(System.in);
+		
+		String [] alunos = new String [10];
+		int[] idades = new int[10];
+		int[] periodo = new int[10];
+		
+		
+		for(int i=0; i<=5 ;i++) {
+			
+			System.out.print("digite os nomes");
+			alunos[i] = x.nextLine();
+			
+			System.out.print("digite as idades");
+			idades[i] = x.nextInt();
+			
+			System.out.print("digite os periodos");
+			periodo[i] = x.nextInt();
+			
+
+			
+		}
+		
+		
+	}
+}
+		
+		
+		
+		
+		
+
+	    
+	
+    	
+    		
+    	
+    	
+    	
+    
+    	
+    	
+	
+	
+			
+	
+	
+	
+
+
